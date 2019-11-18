4f35964c1e8b3c6dc601095352a1ba0df46f1192
Anthony Fojas <anthonyjfojas@gmail.com>
2019-11-17 19:11:22 -0800

    Implement addCard functionality

--- client/components/CardEntry.jsx
+++ client/components/CardEntry.jsx
@@ -13,6 +13,17 @@ class CardEntry extends React.Component {
   }
 
   addCard(event) {
+    event.preventDefault();
+    console.log(this.props.cardInfo);
+    let imageURL = this.props.cardInfo.imageUrl.replace(/(:)/gi, '123abc').replace(/(\/)/gi, '234bcd').replace(/(\.)/gi, '345cde').replace(/(\?)/gi, '456def').replace(/(=)/gi, '567efg').replace(/(&)/gi, '678fgh');
+
+    axios.post(`/api/collection/${this.props.cardInfo.id}/${imageURL}/${this.props.cardInfo.name}`)
+      .then(data => {
+        console.log(this.props.cardInfo.name + ' added');
+      })
+      .catch(error => {
+        console.log(error);
+      })
   }
 
   deleteCard() {
