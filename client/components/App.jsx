cb499b06fdfeeb24fc22c6cc5521337777655b28
Anthony Fojas <anthonyjfojas@gmail.com>
2019-11-24 22:43:44 -0800

    Fix add/delete status message

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -16,6 +16,7 @@ export default function App() {
         {' '}
         <DeleteCollectionBtn />
         {' '}
+        <span id="status"></span>
         <CardList />
       </StateContextProvider>
     </div>
